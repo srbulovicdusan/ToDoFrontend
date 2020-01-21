62c0184762a94582974f1a2194f885dfc5b64b72
Dusan Srbulovic <dsrbulovic@Dusans-MacBook-Pro.local>
2020-01-21 08:32:04 +0100

    Modal za dodavanje i editovanje todo-a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoModal from './components/ToDoModal';
+import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [show, setShow] = useState(false);
+  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleSubmit = (todo) => {console.log(todo); setShow(false)}
+  const todo = {text: "asd asd asd asd asd ", completed : true}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button color="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+      <ToDoModal todo={todo} title="Add todo" show={show} handleClose={handleClose} handleSubmit={handleSubmit}></ToDoModal>
     </div>
   );
 }
